5a5f197d9e51dafae36ae876ef07f9619d44a8ed
DeonWu <DeonWu@b18a5524-d64a-0410-9f42-ad3cd61580fb>
2008-11-16 03:38:21 +0000

    add licenese

--- java/FiveChess/src/org/demo/five/gui/ChessBoard.java
+++ java/FiveChess/src/org/demo/five/gui/ChessBoard.java
@@ -1,3 +1,20 @@
+/* 
+ * Copyright [2008] DeonWu@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *	http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * $ Name LastChangeRevision LastChangeDate LastChangeBy $
+ * $Id$
+ */
+
 package org.demo.five.gui;
 
 import java.awt.Canvas;
@@ -5,7 +22,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 /**
- * ∆Â≈Ã
  * @author deon
  *
  */
@@ -40,7 +56,7 @@ public class ChessBoard extends Canvas{
 		for(int i = 0; i <= 18; i++){
 			int startX = leftPadding + i * cellWidth;
 			int startY = topPadding + i * cellWidth;
-			//ª≠∫·œﬂ
+
 			g.drawString(i + "", startX - 4, topPadding - 8);
 			g.drawLine(leftPadding, startY, endX, startY);
 			
--- java/FiveChess/src/org/demo/five/gui/MainPlane.java
+++ java/FiveChess/src/org/demo/five/gui/MainPlane.java
@@ -1,3 +1,20 @@
+/* 
+ * Copyright [2008] DeonWu@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *	http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * $ Name LastChangeRevision LastChangeDate LastChangeBy $
+ * $Id$
+ */
+
 package org.demo.five.gui;
 
 import java.awt.BorderLayout;
@@ -10,7 +27,7 @@ public class MainPlane extends JFrame {
 	private ChessBoard board = null;
 	
 	public MainPlane(){
-		  super("JavaŒÂ◊”∆ÂøÕªß∂À");
+		  super("Java‰∫îÂ≠êÊ£ã");
 		  setLayout(new BorderLayout());
 		  board = new ChessBoard();
 		  this.getContentPane().add("Center", board);
