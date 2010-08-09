d02f449c7487684eb8ff34981937f427cfab7055
wudalong <wudalong@gmail.com>
2010-08-09 13:40:11 +0000

    import social net project.

---
+++ java/SocialNet/src/org/socialnet/core/DataNode.java
@@ -0,0 +1,7 @@
+package org.socialnet.core;
+
+public class DataNode {
+	public int nodeId;
+	public int[] edges; //related DataNode from current Node.
+
+}
---
+++ java/SocialNet/src/org/socialnet/core/NodeHeap.java
@@ -0,0 +1,23 @@
+package org.socialnet.core;
+
+/**
+ * 数据节点管理，支持动态加载节点关系，使用内存管理，过期节点内存释放, 最近访问缓存
+ * 。
+ * @author Lenovo
+ *
+ */
+
+public class NodeHeap {
+	
+	public NodeHeap(DataSource ds){
+		
+	}
+
+	public DataNode createNode(){
+		return null;
+	}
+	
+	public DataNode fetch(int nodeId){
+		return null;
+	}
+}
