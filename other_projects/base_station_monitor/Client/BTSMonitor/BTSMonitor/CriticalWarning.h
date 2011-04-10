12faeacc5174fe55ebeebb29e7c16b1ba5395278
woshimingong <woshimingong@b18a5524-d64a-0410-9f42-ad3cd61580fb>
2011-04-10 15:48:36 +0000

    严重告警确认同步，以及告警信息初始化

--- other_projects/base_station_monitor/Client/BTSMonitor/BTSMonitor/CriticalWarning.h
+++ other_projects/base_station_monitor/Client/BTSMonitor/BTSMonitor/CriticalWarning.h
@@ -22,4 +22,21 @@ public:
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	CMFCListCtrl  m_lstCriticalWarning;
+
+public:
+	void AddListView(ALARM_COMING_TYPE type); 
+	bool AckedWarning(CString sUUID);
+
+private:
+	CImageList m_imagelist;
+	int m_nCurItem;
+	long m_alarmIndex;
+	bool m_bOnScroll;
+public:
+	afx_msg void OnNMRClickListCriticalWarning(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnContextMenu(CWnd* /*pWnd*/, CPoint /*point*/);
+	afx_msg void OnWarningAck();
+	afx_msg void OnWarningScroolingOff();
+	afx_msg void OnWarningScroolingOn();
+	bool DeleteItemByUUID(CString sUUID);
 };
