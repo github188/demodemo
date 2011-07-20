4c19c706b6404baa26095a6ebc37189b15f8c0bc
DeonWu <DeonWu@b18a5524-d64a-0410-9f42-ad3cd61580fb>
2011-07-20 15:28:55 +0000

    实现了手动处理充值。

--- other_projects/php/Read8_3.5/admin/income_error_fix.php
+++ other_projects/php/Read8_3.5/admin/income_error_fix.php
@@ -3,6 +3,37 @@ if(!defined('IN_Read8') || !defined('IN_Read8_ACP') || !isset($PHP_SELF)) {
         exit('Access Denied (category.inc.php)');
 }
 
+$pid = $_GET['id'];
+if($pid && $_GET['action'] == 'ignore') {
+        $db->single(__TAB_PAY__);
+        $db->update(array(
+                'state'  => 3,
+        ), array(
+                'id'      => $_GET['id'],
+        ));
+}else if($pid && $_GET['action'] == 'ok'){
+    #echo "------------";
+    $db->single(__TAB_PAY__);
+    $trade = $db->selectOne(array(
+            'field' => 'uid, price, vmoney, dateline',
+            'where' => array(
+                    'id'     => $pid,
+                    'active' => 1,
+            ),
+    ));
+    $db->update(array(
+            'state'   => 2,
+            'suctime' => TIMESTAMP,
+    ), array(
+            'id'      => $pid,
+            'active'  => 1,
+    ));
+    $db->execUpBySql('UPDATE '.__TAB_USER__.' SET vmoney = vmoney + '.$trade['vmoney'].' WHERE uid = '.$trade['uid']);    
+}
+        
+
+#        jumpage("?ptype=$ptype&pfile=$pfile", '删除成功');
+
 $tpl = new AOKOTpl('income_error_fix');
 $tpl->assign('records', $db->select(array(
         'from'  => __TAB_PAY__,
