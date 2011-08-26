8c2fc59ac8629f900d8dc25882e9da3b0eeba84b
DeonWu <DeonWu@b18a5524-d64a-0410-9f42-ad3cd61580fb>
2011-08-26 02:00:54 +0000

    实现发送基本信息.

---
+++ other_projects/python/WeiboAds/src/Weibo/coreapp/context_processors.py
@@ -0,0 +1,23 @@
+import re
+
+#from XXXTool.coreapp.models import *
+import logging
+
+def tep_context(request):
+    url_path = request.path.strip("/")
+    url_path = url_path.split("/")[-1]
+    
+    #result = re.search(r"hudson/(?P<hudson_name>[^/]+)/(?P<hudson_view>[^/?]+)", 
+    #                   url_path, re.I)
+    
+    #cate_list = Category.objects.filter(status='active').order_by("view_order")
+
+    #cate_list = Category.load_for_nav(request)
+    
+    #tep = Project.tep_project()
+            
+    return {
+        'cur_user': request.session.get("login_user"),
+        'param': request.REQUEST,
+        'url_path': url_path
+    }
