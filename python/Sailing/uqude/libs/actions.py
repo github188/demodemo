b5425f4a55913a96e660d7002e6f3626a7012c15
wudalong <wudalong@gmail.com>
2012-04-01 05:53:00 +0000

    add unittest to fetch topic info.

--- python/Sailing/uqude/libs/actions.py
+++ python/Sailing/uqude/libs/actions.py
@@ -1,5 +1,6 @@
-
+# -*- coding: utf8 -*-
 import logging
+import re
 
 class LoginUqude(object):
     def __init__(self, ):
@@ -18,4 +19,108 @@ class FetchGuessTopic(object):
         self.logger.info("start fetch topic, url:%s" % url)
         data = http.get(url)
         
+
+ALL_ATTRS = "[^>]+"
+EMPTY = "\s+"
         
+class TopicParser(object):
+    
+    def process(self, html, topic):
+        t = {}
+        t['title'] = self._fetch_topic_title(html)
+        t['images'] = self._fetch_topic_images(html)
+        t['desc'] = self._fetch_topic_desc(html)
+        t['options'] = self._fetch_topic_options(html)
+        
+        t.update(self._fetch_toipc_id(html))
+        
+        return t
+    
+    def _fetch_topic_title(self, data):
+        """
+    <div class="icons icons2">
+
+      <h1>
+
+        许仙不在我们这里，真的不在，白素贞你就收手吧！</h1>
+      """
+
+        pattern = r'<div class="icons icons2">\s+<h1>([^<]+)</h1>'
+        t = re.search(pattern, data, re.S)
+        if t:
+            return t.group(1).strip()
+            
+        #if re.
+    
+    def _fetch_topic_desc(self, data):
+        """<div class="description">
+
+            <p class="f14">
+
+              这是什么体？<br></p>"""
+              
+        pattern = r'<div class="description">\s+<p class="f14">(.*?)</p>\s+<form'
+
+        t = re.search(pattern, data, re.S)
+        if t:
+            return t.group(1).strip()
+
+
+    def _fetch_topic_images(self, data):
+        """
+        <p class="append-img"> <img src="http://static.uqude.com/uploads/v2/0D/20120309/1331277758386_357.300s600s0.jpg" /> </p>
+        """   
+             
+        pattern = r'<p class="append-img">\s+<img\s+src="([^"]+)"\s+/>\s+</p>'
+
+        t = re.search(pattern, data, re.S)
+        if t:
+            return t.group(1).strip()
+        
+    def _fetch_topic_options(self, data):
+        """
+                          <td colspan="2"><div class="model-box model-box-h V174161">
+
+                              <p class="clear"> <span class="left">
+
+                                <input type="radio" value="174161" class="radio" name="optionId"/>
+
+                                淘宝体</span> </p>
+
+                            </div></td>
+
+        """
+        
+        regex = r'<td\s+colspan="2"><div\s+class="model-box.*?">\s+'
+        regex += r'<p class="clear">\s+<span class="left">\s+'
+        regex += r'<input type="radio" value="(\d+)" class="radio" name="optionId"/>'
+        regex += r'(.*?)</span>\s+</p>\s+'
+        regex += r'</div></td>'
+        
+        #print "xxxxxxxxxxxxxx:" + regex 
+        #<input type="hidden" name="coverId" value="37920" id="coverId"/><input type="hidden" name="coverSubjectId" value="234644" id="coverSubjectId"/>
+        options = {}
+        factor = re.compile(regex, re.S)
+        for item in factor.finditer(data):
+            #print item.groups()
+            options[item.group(1)] = {'id':item.group(1), 'desc':item.group(2).strip(), }
+            
+        return options
+    
+    def _fetch_toipc_id(self, data):
+        """<input type="hidden" name="coverId" value="37920" id="coverId"/><input type="hidden" name="coverSubjectId" value="234644" id="coverSubjectId"/>
+        """
+
+        regex = r'<input type="hidden" name="coverId" value="(\d+)" id="coverId"/><input type="hidden" name="coverSubjectId" value="(\d+)" id="coverSubjectId"/>'
+        
+        ids = {}
+        
+        t = re.search(regex, data, re.S)
+        if t:
+            ids['coverId'] = t.group(1)
+            ids['coverSubjectId'] = t.group(2)
+        
+        return ids
+
+
+    
---
+++ python/Sailing/uqude/libs/tests/test_fetch_guss_topic.py
@@ -0,0 +1,46 @@
+# -*- coding: utf8 -*-
+import unittest
+
+import os, sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from actions import TopicParser
+
+base_root = os.path.dirname(os.path.abspath(__file__))
+
+class FetchGuessTopicTestCase(unittest.TestCase):
+    
+    def test_fetch_topic_data(self):
+        topic = TopicParser()
+        
+        data = self._load_data('gest_topic_1.html')
+                
+        t = topic.process(data, {})
+        self.assertEqual(t['title'], "许仙不在我们这里，真的不在，白素贞你就收手吧！", "not found topic title")
+        self.assertEqual(t['images'], "http://static.uqude.com/uploads/v2/0D/20120309/1331277758386_357.300s600s0.jpg", "not found topic images")
+        self.assertEqual(t['desc'], "这是什么体？<br>", "not found topic desc, '%s'" % t['desc'])
+        
+        options = t['options']
+        
+        self.assertEqual(len(options), 4, "the topic len shoud 4, fined:%s" % len(options))
+        
+        
+        self.assertEqual(options['174161']['id'], '174161')
+        self.assertEqual(options['174161']['desc'], '淘宝体')
+        
+        self.assertEqual(options['174163']['desc'], '咆哮体')
+        
+        self.assertEqual(t['coverId'], '37920')
+        self.assertEqual(t['coverSubjectId'], '234644')
+
+
+    def _load_data(self, f):
+        fd = open(os.path.join(base_root, f), 'r')
+        data = fd.read()
+        fd.close()
+        
+        return data
+
+if __name__ == "__main__":
+    unittest.main()
